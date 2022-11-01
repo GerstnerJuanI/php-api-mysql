56ba6860e76ee17eaea81e0dc239f45ffaeed8c8
GerstnerJuanI <43797469+GerstnerJuanI@users.noreply.github.com>
2022-11-01 10:45:15 -0300

    se creo modelo y controlador Categoria

---
+++ Controller/categoria.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once("../Config/conexion.php");
+require_once("../Models/Categoria.php");
+
+$categoria= new Categoria();
+
+switch ($_GET["op"]) {
+    case "GetAll":
+        $datos = $categoria->get_categoria();
+        echo json_encode($datos);
+        break;
+    
+    default:
+        # code...
+        break;
+}
+?>
