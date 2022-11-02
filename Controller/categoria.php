042b9a78ee761b34ce8c90c80540cc76567f9b4d
GerstnerJuanI <43797469+GerstnerJuanI@users.noreply.github.com>
2022-11-02 09:51:37 -0300

    buscar categoria segun su id - POST

--- Controller/categoria.php
+++ Controller/categoria.php
@@ -4,13 +4,16 @@ require_once("../Config/conexion.php");
 require_once("../Models/Categoria.php");
 
 $categoria= new Categoria();
-
+$body = json_decode(file_get_contents("php://input"),true);
 switch ($_GET["op"]) {
     case "GetAll":
         $datos = $categoria->get_categoria();
         echo json_encode($datos);
         break;
-    
+    case "GetId":
+        $datos =$categoria->get_categoria_x_id($body["cat_id"]);
+        echo json_encode($datos);
+
     default:
         # code...
         break;
--- Models/Categoria.php
+++ Models/Categoria.php
@@ -14,6 +14,22 @@ class Categoria extends Conectar
     
         return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
     }
+
+
+    public function get_categoria_x_id($cat_id)
+    {
+        $conectar=parent::conexion();
+    
+        parent::set_name();
+    
+        $sql="SELECT * FROM tm_categoria WHERE est=1 AND 
+        cat_id = ?";
+        $sql =$conectar->prepare($sql);
+        $sql->bindValue(1,$cat_id);
+        $sql->execute();
+    
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
 
 
